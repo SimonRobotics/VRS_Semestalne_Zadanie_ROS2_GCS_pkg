d93052b6b671213589de16b58b824474385af8ea
SimonRobotics <simonmm007@gmail.com>
2026-01-09 09:08:52 +0100

    joy added

--- gcs_bringup/launch/gcs_bringup.launch.py
+++ gcs_bringup/launch/gcs_bringup.launch.py
@@ -1,25 +1,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
-    config = os.path.join(
-        get_package_share_directory('tank_bringup'),
-        'config',
-        'uart_config.yaml'
-    )
-
-    config_cam = os.path.join(
-        get_package_share_directory('tank_bringup'),
-        'config',
-        'camera_config.yaml'
+    config_teleop = os.path.join(
+            get_package_share_directory('my_robot_teleop'),
+            'config',
+            'trustmaster.yaml'
     )
 
     return LaunchDescription([
+        
+        Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            parameters=[config_teleop]
+        ),
+
         Node(
             package='gcs_pkg',
             executable='hub_node',
-        ),
+        )
     ])
